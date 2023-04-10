ac2ad9f27d28982537a5d78d82b5ea6c014bcf69
unknown <nguyentamnhu1221@gmail.com>
2023-04-10 16:42:28 -0400

    ran prettier to fix formatting

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ import restaurantRoutes from './restaurants.js';
 const constructorMethod = (app) => {
   app.use('/', restaurantRoutes);
   app.use('*', (req, res) => {
-    return res.status(404).render('../views/error', {title: "Error 404", error: "Error 404: Webpage Not Found"});
+    return res
+      .status(404)
+      .render('../views/error', { title: 'Error 404', error: 'Error 404: Webpage Not Found' });
   });
 };
 
