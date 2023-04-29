7782fea2b945c1ae1c3ef6d75dc2261df1d85f1d
Kashmeeber <92395069+Kashmeeber@users.noreply.github.com>
2023-04-29 17:53:08 -0400

    Unneeded directory

--- data/index.js
+++
@@ -1,4 +0,0 @@
-// This file should import both data files and export them as shown in the lecture code
-import * as restaurantDataFunctions from './restaurants.js';
-
-export const restaurantData = restaurantDataFunctions;
