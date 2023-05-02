90c1763e46011f642f2a70f56ba4dd90b4167b47
Kashmeeber <areeb.ch2001@gmail.com>
2023-05-02 18:34:16 -0400

    fixed chatbot code

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,32 @@
 function homepageQuestion() {
-  let option1 = document.getElementById('option1');
-  let option2 = document.getElementById('option2');
-  let form = document.getElementById('form');
-  let pTag = document.createElement('p');
-  let option = document.getElementsByName('option');
+  let option1 = document.getElementById('option1').checked;
+  let option2 = document.getElementById('option2').checked;
+  let form = document.getElementById('chatbot-home-form');
+  let pTag = document.getElementById('home-answer');
+  // let option = document.getElementsByName('option');
   if (form) {
     document.addEventListener('submit', function (event) {
       event.preventDefault();
       let divTag = document.getElementById('main');
       pTag.innerHTML = '';
-      let optionValue = null;
-      console.log(optionValue);
-      if (optionValue === null) {
+      if (!option1 && !option2 ) {
         pTag.innerHTML = 'Please select one option';
-      } else if (optionValue !== null) {
-        if (optionValue === 'option1') {
+      } else if (option1) {
           pTag.innerHTML =
             'To use the homepage, type in both the restaurant and location. The results will be displayed matching your request.';
-        } else if (optionValue === 'option2') {
+        } else if (option2) {
           pTag.innerHTML =
             'If you do not know where you want to eat, take our quiz to determine the best restaurant for you!';
         }
-      }
       divTag.appendChild(pTag);
-      setTimeout(function () {
-        pTag.remove(), form.reset();
-      }, 3000);
+      
+      setTimeout(function () { 
+        // form.reset(), 
+        divTag.removeChild(pTag),
+        form.reset()
+        // ,pTag.innerHTML = ''
+        ;
+      }, 5000);
     });
   }
 }
